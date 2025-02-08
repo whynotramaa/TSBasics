7fe4e170d89ff51af00da94c51d0f11dfc740e6a
Rama <ramanaththakur1@gmail.com>
2025-02-09 01:33:29 +0530

    done till interfaces

---
+++ app.ts
@@ -0,0 +1,28 @@
+var a =12
+
+let arr: [number,string] = [3,"kurmi"]
+
+enum UserRoles{
+    ADMIN = "admin",
+    GUEST = "guest",
+    SUPER_ADMIN = "super_admin"
+}
+
+function kurmi9(): string{
+    return "chhakka"
+}
+
+function abcd(x:number,y:String):void{
+    
+}
+
+interface Userr{
+    name: string,
+    email: string,
+    gender ?: string //?: -->> makes this one param optional
+}
+
+function userData(obj: Userr){
+    obj.email = "ramarama@gmail.com",
+
+}
